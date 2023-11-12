1904dd7b6c6c2c2366d26e5cecc3490e8dd2236e
PYJS123 <bibi.abrah@gmail.com>
2023-11-12 20:44:30 +0000

    add most for main

--- main.bat
+++ main.bat
@@ -1,2 +1,18 @@
+@echo off
+
 echo Welcome to snake in the command prompt!
 pause
+cls
+echo Your snake looks like this: [][][]() The head is the ()
+echo When you press enter, you will start the game. Good luck!
+pause
+cls
+
+set width=51
+set height=11
+set /a snakex=(width+1)/2
+set /a snakey=(height+1)/2
+
+FOR /L %i IN (1,1,snakex-1) DO (
+    
+)
